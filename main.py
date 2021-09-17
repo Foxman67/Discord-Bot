b1755028ec84687b0bb61476c80a0e6c26850824
Foxman67 <43353709+Foxman67@users.noreply.github.com>
2021-09-16 23:52:08 -0300

    fixup! started on main cog commands
    Signed-off-by: Foxman67 <43353709+Foxman67@users.noreply.github.com>

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import discord
-
+import json
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix="$")
@@ -14,5 +14,20 @@ class MyClient(discord.Client):
         print("up and running {0} ".format(self.user))
 
 
+
+class IDC(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+   # @commands.command()
+    #async def add_ID(self,ctx,game,ID):
+
+
+
+
+with open('token.txt') as t:
+    token = t.readline()
+
+
 client = MyClient()
-client.run("")
+client.run(token)
